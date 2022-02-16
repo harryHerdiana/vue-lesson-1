f54141a8d5a70b8e1eba2cf4800736a62cf743d0
Harry Herdiana <harry.gamebot@gmail.com>
2022-02-16 22:10:16 +0700

    first commit

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="nameInput">Name</label>
+      <input v-model="enteredName" name="nameInput" type="text" />
+    </div>
+    <div>
+      <label for="phoneInput">Phone</label>
+      <input v-model="enteredPhone" name="phoneInput" type="text" />
+    </div>
+    <div>
+      <label for="emailInput">E-Mail</label>
+      <input v-model="enteredEmail" name="emailInput" type="text" />
+    </div>
+    <button>Add contact</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
